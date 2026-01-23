be4ed20f7e3a63084629e1208d596b90b5a365d5
Haz <haziqaiman2@gmail.com>
2026-01-23 17:51:28 +0800

    initial api routes and models

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\InquiryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('answers', AnswerController::class);
+Route::resource('inquiries', InquiryController::class);
