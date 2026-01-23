8835656e7ffdbde88f95c744f4414292ce7c53bf
Haz <haziqaiman2@gmail.com>
2026-01-23 22:11:30 +0800

    initial migrations for the initial models

---
+++ database/migrations/2026_01_23_120926_create_inquiries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Item;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignIdFor(Item::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(User::class)->nullable()->constrained()->nullOnDelete();
+
+            $table->geometry('location', subtype: 'point');
+            $table->unsignedMediumInteger('search_radius_meters'); // Max 80km if not anywhere
+
+            // flags
+            $table->unsignedTinyInteger('anywhere')->default(0);
+            $table->unsignedTinyInteger('same_country_only')->default(1);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inquiries');
+    }
+};
