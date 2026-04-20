b4af298189952ef63e2b81326950fbd85f71fa80
Haz <haziqaiman2@gmail.com>
2026-04-20 13:35:51 +0800

    added helper function to create Point with the SRID inside the base Controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use MatanYadaev\EloquentSpatial\Objects\Point;
+
 abstract class Controller
 {
-    //
+    /**
+     * Creates a point object with SRID 4326 for the given latitude and longitude.
+     */
+    static function createPoint($latitude, $longitude)
+    {
+        return new Point($latitude, $longitude, 4326);
+    }
 }
