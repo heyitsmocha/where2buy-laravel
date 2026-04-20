e503b24c02dd888c1d2a937d83a9adf8f74b7aac
Haz <haziqaiman2@gmail.com>
2026-04-20 13:53:37 +0800

    inquiry controller: initial store implementation

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Inquiry;
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class InquiryController extends Controller
@@ -20,7 +21,42 @@ class InquiryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            // Data for the inquiry
+            'latitude' => 'required|numeric|between:-90,90',
+            'longitude' => 'required|numeric|between:-180,180',
+            'search_radius_meters' => 'required|integer|min:0|max:80000',
+            'anywhere' => 'boolean',
+            'same_country_only' => 'boolean',
+
+            // If item_id is not provided, name is required to create/find item
+            'item_id' => 'required_without:name|integer',
+            'name' => 'required_without:item_id|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        // If item_id is provided, use it. Otherwise, create/find item by name.
+        if ($request->filled('item_id')) {
+            $item = Item::findOrFail($request->input('item_id'));
+        } elseif ($request->filled('name')) {
+            $item = Item::firstOrCreate(
+                ['name' => $request->input('name')],
+                ['description' => $request->input('description', '')]
+            );
+        } else {
+            abort(400, 'Either item_id or name must be provided.');
+        }
+
+        // Create the inquiry with the item_id
+        $inquiry = $request->user()->inquiries()->create([
+            'item_id' => $item->id,
+            'location' => $this->createPoint($request->input('latitude'), $request->input('longitude')),
+            'search_radius_meters' => $request->input('search_radius_meters'),
+
+            'anywhere' => $request->input('anywhere', false),
+            'same_country_only' => $request->input('same_country_only', true)
+        ]);
+        return $inquiry;
     }
 
     /**
