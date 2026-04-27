775c42fcc4f1ffca6b5c3d88da08b3ecf14521ef
Haz <haziqaiman2@gmail.com>
2026-04-27 13:10:55 +0800

    Controller: added helper method calculateDistanceMeters()

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,4 +13,29 @@ abstract class Controller
     {
         return new Point($latitude, $longitude, 4326);
     }
+
+    /**
+     * Calculates the distance in meters between two points using the Haversine formula.
+     */
+    static function calculateDistanceMeters(Point $point1, Point $point2)
+    {
+        // Haversine formula to calculate distance in meters between two points
+        $earthRadius = 6371000; // Earth radius in meters
+
+        $latFrom = deg2rad($point1->latitude);
+        $lonFrom = deg2rad($point1->longitude);
+        $latTo = deg2rad($point2->latitude);
+        $lonTo = deg2rad($point2->longitude);
+
+        $latDelta = $latTo - $latFrom;
+        $lonDelta = $lonTo - $lonFrom;
+
+        $a = sin($latDelta / 2) * sin($latDelta / 2) +
+            cos($latFrom) * cos($latTo) *
+            sin($lonDelta / 2) * sin($lonDelta / 2);
+
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+        return round($earthRadius * $c);
+    }
 }
