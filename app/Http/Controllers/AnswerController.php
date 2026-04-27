2f85fb7f96860e1e8fd79b76aa27f83a723a3f6b
Haz <haziqaiman2@gmail.com>
2026-04-27 13:12:00 +0800

    AnswerController: store implementation, initial show, update, and destroy implementation

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -45,30 +45,75 @@ class AnswerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'inquiry_id' => 'required|integer|exists:inquiries,id',
+            'latitude' => 'required|numeric|between:-90,90',
+            'longitude' => 'required|numeric|between:-180,180',
+            'store_name' => 'required|string|max:255',
+            'store_address' => 'string|max:500',
+            'additional_info' => 'nullable|string|max:1000',
+        ]);
+
+        $inquiry = Inquiry::findOrFail($request->input('inquiry_id'));
+        $location = $this->createPoint($request->latitude, $request->longitude);
+
+        if ($this->calculateDistanceMeters($location, $inquiry->location) > $inquiry->search_radius_meters) {
+            return response()->json(['error' => 'Answer location is outside the search radius of the inquiry'], 400);
+        }
+
+        $answer = $inquiry->answers()->create([
+            'user_id' => $request->user()->id,
+            'store_name' => $request->store_name,
+            'store_address' => $request->store_address,
+
+            'location' => $location
+        ]);
+
+        return $answer->toResource();
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Answer $answer)
     {
-        //
+        return $answer->toResource();
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Answer $answer)
     {
-        //
+        $request->validate([
+            'latitude' => 'required|numeric|between:-90,90',
+            'longitude' => 'required|numeric|between:-180,180',
+            'store_name' => 'required|string|max:255',
+            'store_address' => 'required|string|max:500',
+            'additional_info' => 'nullable|string|max:1000',
+        ]);
+
+        $location = $this->createPoint($request->latitude, $request->longitude);
+        $request->merge(['user_id' => $request->user()->id, 'location' => $location]);
+
+        $answer->update($request->all());
+
+        return $answer->toResource();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Answer $answer)
     {
-        //
+        return Answer::destroy($answer->id);
+    }
+
+    /**
+     * Get answers made by the authenticated user.
+     */
+    public function myAnswers(Request $request)
+    {
+        return $request->user()->answers()->get()->toResourceCollection();
     }
 }
