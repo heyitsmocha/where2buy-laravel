9b5d86fb9272e1c97b845e6495d309dc80bb520e
Haz <haziqaiman2@gmail.com>
2026-05-26 13:31:32 +0800

    added api fetch utility functions

---
+++ resources/js/util.ts
@@ -0,0 +1,54 @@
+/**
+ * Utility functions for making API requests.
+ *
+ * url: The endpoint URL for the API request. This should be relative to the base API URL (e.g., 'inquiries/1').
+ * options: Optional configuration for the fetch request, such as method, headers, body, etc.
+ * onData: A callback function that will be called with the parsed JSON data if the request is successful.
+ * onError: A callback function that will be called with the error object if the request fails.
+ */
+export async function apiGet<T = any>({ url, options, onData, onError }: {
+  url: string,
+  options?: RequestInit,
+  onData?: (data: T[]) => void,
+  onError?: (error: any) => void,
+}
+): Promise<T[]> {
+  return fetch(`/api/${url}`, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      ...options?.headers,
+    },
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  }).then(data => {
+    console.log('API response data:', data);
+    onData?.(data.data);
+    return data;
+  }).catch(error => {
+    console.error('Error fetching JSON:', error);
+    onError?.(error);
+    throw error;
+  });
+}
+
+export async function apiPost<T = any>({ url, body, onData, onError }: {
+  url: string,
+  body: any,
+  onData?: (data: T[]) => [void],
+  onError?: (error: any) => void,
+}): Promise<T[]> {
+  return apiGet<T>({
+    url,
+    options: {
+      method: 'POST',
+      body: JSON.stringify(body),
+    },
+    ...(onData && { onData }),
+    ...(onError && { onError }),
+  });
+}
