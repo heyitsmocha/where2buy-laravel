8053e8cdf3704e4086693f8d91d14128e5fb2372
Haz <haziqaiman2@gmail.com>
2026-06-03 14:06:24 +0800

    AppSidebar: use icons, track active page, update ui based on login

--- resources/js/Components/AppSidebar.tsx
+++ resources/js/Components/AppSidebar.tsx
@@ -4,29 +4,80 @@ import {
   SidebarFooter,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from "@/Components/ui/sidebar";
+
+import { Button } from "@/Components/ui/button";
+import { Link } from "@inertiajs/react";
+
+import { LuLogIn, LuLogOut } from "react-icons/lu";
+import { MdHome, MdListAlt } from "react-icons/md";
+
+import { usePage } from "@inertiajs/react";
 
 interface AppSidebarProps {
   isLoggedIn: boolean;
   onLoginClick: () => void;
   onLogoutClick: () => void;
 }
+
 export default function AppSidebar({ isLoggedIn, onLoginClick, onLogoutClick }: AppSidebarProps) {
 
+  const { url } = usePage();
+
+  const isUrlActive = (path: string) => {
+    return url === path;
+  }
+
   return (
-    <Sidebar variant="sidebar">
-      <SidebarHeader>
-        <SidebarMenu className="text-center">
-          <SidebarMenuItem>Where2Buy</SidebarMenuItem>
-          <SidebarMenuItem className="text-gray-600">Find where to buy your items!</SidebarMenuItem>
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="bg-fuchsia-200 h-32 mb-4 rounded-b-lg">
+        <SidebarMenu className="pt-4">
+          <SidebarMenuItem>Guest</SidebarMenuItem>
+          <SidebarMenuItem>{"email goes here"}</SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <p className="text-gray-600"></p>
+        <Link href="/">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton className="p-4 cursor-pointer" isActive={isUrlActive("/")} tooltip="Home">
+                <MdHome className="inline-block mr-2" />
+                <span>Home</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </Link>
+        { !isLoggedIn
+          ? <p className="text-gray-600">Please log in for full access.</p>
+          : <>
+            <Link href="/inquiries/me">
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton className="p-4 cursor-pointer" isActive={isUrlActive("/inquiries/me")} tooltip="My Inquiries">
+                    <MdListAlt className="inline-block mr-2" />
+                    <span>My Inquiries</span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </Link>
+          </>
+        }
       </SidebarContent>
       <SidebarFooter>
-        <p className="text-sm text-gray-500">&copy; Where2Buy</p>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton variant="outline" onClick={isLoggedIn ? onLogoutClick : onLoginClick} className={`p-4 cursor-pointer justify-center ${isLoggedIn ? "bg-red-500 hover:bg-red-600" : "bg-fuchsia-700 hover:bg-fuchsia-600 active:bg-fuchsia-800 text-white hover:text-white active:text-white"}`}>
+              {/* <Button onClick={isLoggedIn ? onLogoutClick : onLoginClick} className={`cursor-pointer ${isLoggedIn ? "bg-red-500 hover:bg-red-600" : "hover:bg-fuchsia-600 active:bg-fuchsia-800"}`}> */}
+              {isLoggedIn ? <LuLogOut /> : <LuLogIn />}
+              <span>{isLoggedIn ? "Logout" : "Login"}</span>
+              {/* </Button> */}
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        {/* <p className="text-sm text-gray-500">&copy; Where2Buy</p> */}
       </SidebarFooter>
     </Sidebar>
   )
