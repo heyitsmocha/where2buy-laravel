02129a4799089aef0ee19dd44d9b28022f496926
Haz <haziqaiman2@gmail.com>
2026-06-09 12:40:58 +0800

    MapComponent: cluster up nearby markers

--- resources/js/Components/Map/MapComponent.tsx
+++ resources/js/Components/Map/MapComponent.tsx
@@ -8,15 +8,24 @@ import {
 import MapController from './MapController';
 import type { MapComponentProps } from './map.types';
 
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+
 export default function MapComponent({ initialCoordinates, initialZoom, circles, markers, ...controllerProps }: MapComponentProps) {
   // console.log('initialCoordinates:', initialCoordinates, 'initialZoom:', initialZoom);
+  const leafletBaseLink = "https://unpkg.com/leaflet@1.9.4/dist";
+
   return (
     <>
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+      <link rel="stylesheet" href={`${leafletBaseLink}/leaflet.css`}
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossOrigin="" />
 
-      <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/react-leaflet-markercluster/styles"
+      />
+
+      <script src={`${leafletBaseLink}/leaflet.js`}
         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
         crossOrigin=""></script>
 
@@ -28,16 +37,18 @@ export default function MapComponent({ initialCoordinates, initialZoom, circles,
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-        {markers?.map(marker => {
+        <MarkerClusterGroup>
+          {markers?.map(marker => {
             console.log('Rendering marker:', marker);
             return (
-                <Marker key={marker.id} position={marker.location} >
-                    <Popup>
-                        {marker.store_name}
-                    </Popup>
-                </Marker>
+              <Marker key={marker.id} position={marker.location} >
+                <Popup>
+                  {marker.store_name}
+                </Popup>
+              </Marker>
             );
-        })}
+          })}
+        </MarkerClusterGroup>
 
         <MapController
           {...controllerProps}
