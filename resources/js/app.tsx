3a0a1cbb9bbcf038786ed6c76423e2fe2a639417
Haz <haziqaiman2@gmail.com>
2026-06-10 14:22:04 +0800

    simplified some config files - tsconfig: unspecify file type - app.tsx:  use resolvePageComponent

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,21 +1,16 @@
 import './bootstrap.js';
 import '../css/app.css'; // Import here instead of vite.config.js (ref: https://laravel.com/docs/12.x/vite#configuring-vite)
 
-import { createInertiaApp } from '@inertiajs/react'
+import { createInertiaApp, type ResolvedComponent } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import type { ComponentType } from 'react';
 
 createInertiaApp({
     resolve: (name) =>
-        // resolvePageComponent(
-        //     `./Pages/${name}.tsx`,
-        //     import.meta.glob('./Pages/**/*.tsx'),
-        // ) as unknown as ComponentType,
-        {
-            const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true });
-            return pages[`./Pages/${name}.tsx`] as ComponentType;
-        },
+        resolvePageComponent<ResolvedComponent>(
+            `./Pages/${name}.tsx`,
+            import.meta.glob<ResolvedComponent>('./Pages/**/*.tsx'),
+        ) ,
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     }
