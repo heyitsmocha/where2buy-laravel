55a3f484f6b078836d17b155ca2afe2301955af1
Haz <haziqaiman2@gmail.com>
2026-06-13 16:19:54 +0800

    MapComponent: changed tile provider to stadiamaps

--- resources/js/Components/Map/MapComponent.tsx
+++ resources/js/Components/Map/MapComponent.tsx
@@ -35,7 +35,9 @@ export default function MapComponent({ initialCoordinates, initialZoom, circles,
         style={{ width: '100%', height: '500px' }}
         whenReady={() => { }}
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
 
         <MarkerClusterGroup>
           {markers?.map(marker => {
